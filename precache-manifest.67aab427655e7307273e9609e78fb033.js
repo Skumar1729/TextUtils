e408459845bd7f6b2b49c80d350ec030956201af
Skumar1729 <sanjaymrj1729@gmail.com>
2022-08-15 14:17:19 +0530

    Updates

---
+++ precache-manifest.67aab427655e7307273e9609e78fb033.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b1bb76c2ca48bc88427",
+    "url": "/TextUtils-React/static/css/main.657a82cd.chunk.css"
+  },
+  {
+    "revision": "4b1bb76c2ca48bc88427",
+    "url": "/TextUtils-React/static/js/main.4b1bb76c.chunk.js"
+  },
+  {
+    "revision": "cc360fb6938c4a946641",
+    "url": "/TextUtils-React/static/js/1.cc360fb6.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtils-React/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "2499cddb344818b4039d",
+    "url": "/TextUtils-React/static/js/runtime~main.2499cddb.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/TextUtils-React/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "6b955b9a401a29ac7c3426c06a12cf2d",
+    "url": "/TextUtils-React/index.html"
+  }
+];
